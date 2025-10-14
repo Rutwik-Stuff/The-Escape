894293858c9752fbefc9e72519f4c0c952cf8b1c
KCheburek <rafael.rizoev@gmail.com>
2025-10-14 17:05:35 -0500

    wall jump (right)

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -14,29 +14,52 @@ public class Movement : MonoBehaviour
     public GameObject right;
     private bool isHitting;
     private static bool isRight;
+    public bool isOnWall = false;
+    public bool isRightWall;
+    private bool disableRight;
+    private bool disableLeft;
+    private bool wallJump;
 
     void Update()
     {
-        if(isOnGround || isJumping){
-            if (Input.GetKey(KeyCode.Space)) 
-        {
-            if(!isJumping) time = DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond;
-            isJumping = true;
-            if(DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond-time<500){
-                var v = rb.linearVelocity;
-                v.y = jumpVelocity;   
-                rb.linearVelocity = v;
+        if(isOnGround || isJumping || isOnWall){
+            if (Input.GetKey(KeyCode.Space)){
+                if(!isJumping) time = DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond;
+                    isJumping = true;
+                    if(DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond-time<500){   
+                        if(isOnWall && !isOnGround || wallJump){
+                            wallJump = true;
+                            if(DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond-time<100){
+                                 disableLeft = true;
+                            } else {
+                                disableLeft = false;
+                            }
+                            var v = rb.linearVelocity;
+                            v.y = jumpVelocity;   
+                            v.x = 12f;
+                            rb.linearVelocity = v;
+                        } else {
+                            var v = rb.linearVelocity;
+                            v.y = jumpVelocity;   
+                            rb.linearVelocity = v;
+                            disableLeft = false;
+                            disableRight = false;
+                        }
+                    } else {
+                        isJumping = false;
+                        wallJump = false;
+                    }
+            
             } else {
                 isJumping = false;
+                wallJump = false;
+                disableLeft = false;
+                disableRight = false;
             }
-            
-        } else {
-            isJumping = false;
-        }
         }
         
-        if (Input.GetKey(KeyCode.D)) 
-        {
+        if (Input.GetKey(KeyCode.D) && !disableRight){
+        
             isRight = true;
             var v = rb.linearVelocity;
             v.x = walkVelocity;   
@@ -47,8 +70,8 @@ public class Movement : MonoBehaviour
                     right.SetActive(true);
                 }
             }
-        } else if (Input.GetKey(KeyCode.A)) 
-        {
+        } else if (Input.GetKey(KeyCode.A) && !disableLeft){
+        
             isRight = false;
             var v = rb.linearVelocity;
             v.x = -walkVelocity;   
@@ -83,11 +106,20 @@ public class Movement : MonoBehaviour
                 }
             }
         }
+        if(!isOnGround && isOnWall){
+            rb.linearDamping = 9f;
+        } else {
+            rb.linearDamping = 1f;
+        }
     }
     public void onGround(bool state){
         isOnGround = state;
     }
     public void setHitting(bool state){
         isHitting = state;
+    }
+    public void setOnWall(bool state, bool wallKind){
+        isOnWall = state;
+        isRightWall = wallKind;
     }
 }
